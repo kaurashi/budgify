d12657f9f35063b5c30951a1c2661183d713c58e
Ashmeet Kaur <ashmeetkaurashu2006@gmail.com>
2026-03-12 22:20:32 +0530

    Add expense APIs (POST /add-expense, GET /expenses)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/" , (req,res) =>{
-    res.send("congratulations backend running!");
+app.use(express.json());
+
+let expense =[];
+
+app.post("/add-expense", (req, res) =>{
+    const  {description, amount} = req.body;
+
+ const newExpense ={
+    id: expense.length+1,
+    description,
+    amount
+ };
+
+ expense.push(newExpense);
+  
+
+res.json({
+    message:"successfully expense added!",
+    expense:newExpense
+  });
 });
 
-app.listen(5000 ,() =>{
-    console.log("server running on port 5000!")
+app.listen(5000, ()=>{
+    console.log("successfully expense added!");
 });
 
 
