5e568166f8997d1b6248134b9950e1861b905bc2
Ashmeet Kaur <ashmeetkaurashu2006@gmail.com>
2026-06-27 12:09:28 +0530

    fix: use environment port for deployment

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,8 @@ app.get("/", (req, res) => {
   res.send("backend is working");
 });
 
-app.listen(5000, () => {
-  console.log("server running on port 5000!");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}!`);
 });
